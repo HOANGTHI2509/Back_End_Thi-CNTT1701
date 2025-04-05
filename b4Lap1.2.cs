c8eb006c6d65f9680ccf1a23303558434d54ec0b
HOANGTHI2509 <160563368+HOANGTHI2509@users.noreply.github.com>
2025-04-05 21:52:35 +0700

    Add files via upload

---
+++ b4Lap1.2.cs
@@ -0,0 +1,34 @@
+﻿//Bài 4: Viết hàm để tìm số lớn thứ hai trong một mảng các số nguyên. 
+
+Console.OutputEncoding = System.Text.Encoding.UTF8; // lenh go tieng viet
+int n;
+Console.Write("Nhập số phần tử của mảng: ");
+n = int.Parse(Console.ReadLine() ?? "0");
+int[] arr = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Nhập phần tử thứ {i + 1}: ");
+    arr[i] = int.Parse(Console.ReadLine() ?? "0");
+}
+int max1 = int.MinValue;
+int max2 = int.MinValue;
+foreach (int i in arr)
+{
+    if (i > max1)
+    {
+        max2 = max1;
+        max1 = i;
+    }
+    else if (i > max2 && i != max1)
+    {
+        max2 = i;
+    }
+}
+if (max2 == int.MinValue)
+{
+    Console.WriteLine("Không có số lớn thứ hai trong mảng.");
+}
+else
+{
+    Console.WriteLine($"Số lớn thứ hai trong mảng là: {max2}");
+}
---
+++ b6Lap1.2.cs
@@ -0,0 +1,38 @@
+﻿//Bài 6: Viết hàm sắp xếp mảng số thực n phần tử nhập từ bàn phím theo chiều tăng dần.
+static void SapXep(float[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                float temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+Console.OutputEncoding = System.Text.Encoding.UTF8; // lenh go tieng viet
+int n;
+Console.Write("Nhập số phần tử của mảng: ");
+n = int.Parse(Console.ReadLine() ?? "0");
+float[] arr = new float[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Nhập phần tử thứ {i + 1}: ");
+    arr[i] = float.Parse(Console.ReadLine() ?? "0");
+}
+Console.WriteLine("Mảng trước khi sắp xếp:");
+foreach (float i in arr)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+SapXep(arr);
+Console.WriteLine("Mảng sau khi sắp xếp theo chiều tăng dần:");
+foreach (float i in arr)
+{
+    Console.Write(i + " ");
+}
